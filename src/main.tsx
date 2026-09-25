4821c461498d35fad0f77fc4417e8116b96f1113
alco-uurimis <sofijatsaika@gmail.com>
2026-09-25 23:25:59 +0300

    Remove scroll progress runtime component

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ScrollProgress from './components/ScrollProgress';
 import './styles.css';
 import './features.css';
 import './visuals.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><ScrollProgress/><App /></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
