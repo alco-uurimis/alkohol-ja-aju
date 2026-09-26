e38e2f5f2c3f5d3d1576e30fdfda93db11394d64
alco-uurimis <sofijatsaika@gmail.com>
2026-09-26 09:42:22 +0300

    Load clarity styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,4 +7,5 @@ import './visuals.css';
 import './polish.css';
 import './lab.css';
 import './survey.css';
+import './clarity.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
