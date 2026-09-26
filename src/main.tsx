347d23ec97830bd5f1795c71f190f6d249fa1ef5
alco-uurimis <sofijatsaika@gmail.com>
2026-09-26 11:05:05 +0300

    Load real scroll progress tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './scrollProgress';
 import './styles.css';
 import './features.css';
 import './visuals.css';
