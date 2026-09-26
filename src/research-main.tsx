9f9dbb5d4736e5b3413a98129600cd26ddd77652
alco-uurimis <sofijatsaika@gmail.com>
2026-09-26 16:35:16 +0300

    Load final research page polish

--- src/research-main.tsx
+++ src/research-main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ResearchApp from './ResearchApp';
 import './research.css';
+import './research-final.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><ResearchApp /></React.StrictMode>,
